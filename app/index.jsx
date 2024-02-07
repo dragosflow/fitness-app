998118794e91d03a46372457e744985b322e3d16
dragos <dragosflow@yahoo.com>
2024-02-07 16:41:49 +0200

    chore: setup project

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Index(){
+
+    return(
+        <View>
+            <Text className='bg-red-300'>
+                Hello
+            </Text>
+        </View>
+    );
+
+};
