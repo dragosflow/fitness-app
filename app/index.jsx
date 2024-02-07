c23d03ed0e984374c7c93bea840ba58c9ca5d119
dragos <dragosflow@yahoo.com>
2024-02-07 20:30:12 +0200

    chore: create welcome page and add assets

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,42 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text } from "react-native";
-
+import { View, Text, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import Animated, { FadeInUp, FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
 export default function Index(){
 
+    const router = useRouter();
     return(
-        <View>
-            <Text className='bg-red-300'>
-                Hello
-            </Text>
+        <View className='flex-1 flex justify-end'>
+            <StatusBar style="light"/>
+            <Image className="h-full w-full absolute" source={require('../assets/images/welcome.jpg')}/>
+            <LinearGradient 
+                colors={['transparent', '#18181b']}
+                style={{width: wp(100), height: hp(70)}}
+                start={{x:0.5, y:0}}
+                end={{x:0.5, y:0.8}}
+                className='flex justify-end pb-12 space-y-8'
+            >
+                <Animated.View entering={FadeInUp.delay(100).springify()} className='flex items-center'>
+                    <Text style={{fontSize: hp(3.5)}} className='text-white font-bold tracking-wide'>
+                        WORKOUT <Text className="text-red-500">FLOW</Text>
+                    </Text>
+                </Animated.View>
+                <Animated.View entering={FadeInDown.delay(100).springify()}>
+                    <TouchableOpacity
+                    onPress={() => router.push('home')}
+                        style={{height: hp(5), width: wp(80)}} 
+                        className='bg-red-500 rounded-lg flex mx-auto items-center justify-center border-[1px] border-white'
+                    >
+                        <Text style={{fontSize: hp(3)}} className="font-bold text-white tracking-wide ">
+                            Get started
+                        </Text>
+                    </TouchableOpacity>
+                </Animated.View>
+            </LinearGradient>
         </View>
     );
-
 };
---
+++ constants/index.js
@@ -0,0 +1,7 @@
+export const sliderImages = [
+    require('../assets/images/back.jpg'),
+    require('../assets/images/chest.jpg'),
+    require('../assets/images/arms.jpg'),
+    require('../assets/images/legs.jpg'),
+    require('../assets/images/cardio.jpg'),
+];
