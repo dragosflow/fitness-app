aa7159320d139524d80a9211c28d455ca70feaa8
dragos <dragosflow@yahoo.com>
2024-02-08 14:00:44 +0200

    chore: add auto scroll for carousel

--- components/slider.jsx
+++ components/slider.jsx
@@ -1,81 +1,89 @@
-import React, {useRef} from 'react';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
-
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Animated,
-  Dimensions,
-  Image,
-  FlatList,
-  Text,
-  View,
-} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Animated, View, Dimensions, Image, FlatList } from 'react-native';
 import { sliderImages } from '../constants';
 
-const width = wp(100);
+const width = Dimensions.get('window').width;
 const IMG_WIDTH = width * 0.90;
 const IMG_HEIGHT = IMG_WIDTH * 0.70;
 
-const img = sliderImages;
-
-const resources = img.map((image, index)=> ({
+const resources = sliderImages.map((image, index) => ({
   key: String(index),
-  photo: image
-}))
+  photo: image,
+}));
 
 const Slider = () => {
-  const scrollX = useRef(new Animated.Value(0)).current
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const flatListRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [userScrolled, setUserScrolled] = useState(false);
+  const intervalRef = useRef(null);
 
+  const scrollToNextSlide = () => {
+    const nextIndex = currentIndex + 1 >= resources.length ? 0 : currentIndex + 1;
+    flatListRef.current?.scrollToIndex({ animated: true, index: nextIndex });
+    setCurrentIndex(nextIndex);
+  };
+
+  useEffect(() => {
+    intervalRef.current = setInterval(scrollToNextSlide, 5000);
+
+    return () => clearInterval(intervalRef.current);
+  }, [currentIndex, userScrolled]);
+
+  const handleScroll = (event) => {
+    const scrollPosition = event.nativeEvent.contentOffset.x;
+    const currentSlideIndex = Math.round(scrollPosition / width);
+
+    if (currentSlideIndex !== currentIndex) {
+      setUserScrolled(true);
+      setCurrentIndex(currentSlideIndex);
+      clearInterval(intervalRef.current);
+      intervalRef.current = setInterval(scrollToNextSlide, 5000);
+    }
+  };
 
   return (
-      <View >
-        <Animated.FlatList
-            data={resources}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            keyExtractor={(item) => item.key}
-            pagingEnabled
-            onScroll={Animated.event(
-            [{nativeEvent: {contentOffset: {x: scrollX}}}],
-            {useNativeDriver: true},
-            )}
-            renderItem={({item, index}) => {
-            const inputRange = [
-                (index - 1) * width,
-                index * width,
-                (index + 1) * width
-            ];
-            const translateX = scrollX.interpolate({
-                inputRange,
-                outputRange: [-width * .9, 0, width * .7]
-            })
+    <View>
+      <Animated.FlatList
+        ref={flatListRef}
+        data={resources}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.key}
+        pagingEnabled
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true, listener: handleScroll }
+        )}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+          const translateX = scrollX.interpolate({
+            inputRange,
+            outputRange: [-IMG_WIDTH * 0.7, 0, IMG_WIDTH * 0.7], // Ajustează aceste valori pentru efectul de paralax dorit
+          });
           return (
-            <View style={{width, justifyContent: "center", alignItems: "center"}}> 
-                <View style={{width: IMG_WIDTH, borderRadius: 15, height: IMG_HEIGHT, overflow: "hidden", alignItems: "center" }}>
-                    <Animated.Image
-                        source={item.photo}
-                        style={{width: IMG_WIDTH, height: IMG_HEIGHT, resizeMode: "cover", transform: [ {translateX}]}}
-                    />
-                </View>
-           </View>
-
-            )}}
-        />
-
-      </View>
+            <View style={{ width, justifyContent: "center", alignItems: "center" }}> 
+              <View style={{ width: IMG_WIDTH, height: IMG_HEIGHT, overflow: 'hidden', borderRadius: 15, alignItems: "center" }}>
+                <Animated.Image
+                  source={item.photo}
+                  style={{
+                    width: IMG_WIDTH,
+                    height: IMG_HEIGHT,
+                    transform: [{ translateX }],
+                  }}
+                  resizeMode="cover"
+                />
+              </View>
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default Slider;
