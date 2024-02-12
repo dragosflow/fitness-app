f01d8fecbc9f0575c4f66a064e316ae766302194
dragos <dragosflow@yahoo.com>
2024-02-12 18:56:35 +0200

    chore: finish all screens

--- app/exerciseDetails.jsx
+++ app/exerciseDetails.jsx
@@ -1,56 +1,55 @@
-import { View, Text,Image } from "react-native";
+import { View,Text,Image } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import { ScrollView } from "react-native-gesture-handler";
+import Animated, {FadeInDown, FadeInUp} from "react-native-reanimated";
 
 export default function exerciseDetails() {
 
     const item = useLocalSearchParams();
-    // console.log(item)
-
 
     return (
         <View className="flex flex-1">
-            <View className='shadow-md bg-neutral-200 rounded-b-lg'>
+            <Animated.View entering = {FadeInUp.delay(300).springify()}  className='shadow-md bg-neutral-200 rounded-b-lg'>
                 <Image 
                     source={{uri: item.gifUrl}}
                     content-fit='cover'
                     style={{width: wp(100), height: wp(100)}}
                     className='rounded-b-xl'
                 />
-            </View>
+            </Animated.View>
 
             <ScrollView className='mx-4 space-y-2 mt-3' showsVerticalScrollIndicator={false}>
                 
-                <Text style={{fontSize: hp(3)}} className='font semibold text-neutral-800 tracking-wide mb-3 mt-4'>
+                <Animated.Text entering = {FadeInDown.delay(300).springify()} style={{fontSize: hp(3)}} className='font semibold text-neutral-800 tracking-wide mb-3 mt-4'>
                     {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
-                </Text>
-                <Text style={{fontSize: hp(2)}} className='text-neutral-700 tracking-wide mb-1'>
-                    Equipment <Text className='font-bold text-neutral-800'>
+                </Animated.Text>
+                <Animated.Text entering = {FadeInDown.delay(300).springify()} style={{fontSize: hp(2)}} className='text-neutral-700 tracking-wide mb-1'>
+                    Equipment <Animated.Text entering = {FadeInDown.delay(300).springify()} className='font-bold text-neutral-800'>
                         {item?.equipment}
-                    </Text>
-                </Text>
-                <Text style={{fontSize: hp(2)}} className='text-neutral-700 tracking-wide mb-1'>
-                    Secondary Muscles : <Text className='font-bold text-neutral-800'>
+                    </Animated.Text>
+                </Animated.Text>
+                <Animated.Text entering = {FadeInDown.delay(300).springify()} style={{fontSize: hp(2)}} className='text-neutral-700 tracking-wide mb-1'>
+                    Secondary Muscles : <Animated.Text entering = {FadeInDown.delay(300).springify()} className='font-bold text-neutral-800'>
                         {item?.secondaryMuscles}
-                    </Text>
-                </Text>
-                <Text style={{fontSize: hp(2)}} className='text-neutral-700 tracking-wide mb-2'>
-                    Target Muscle : <Text className='font-bold text-neutral-800'>
+                    </Animated.Text>
+                </Animated.Text>
+                <Animated.Text entering = {FadeInDown.delay(300).springify()} style={{fontSize: hp(2)}} className='text-neutral-700 tracking-wide mb-2'>
+                    Target Muscle : <Animated.Text entering = {FadeInDown.delay(300).springify()} className='font-bold text-neutral-800'>
                         {item?.target}
-                    </Text>
-                </Text>
+                    </Animated.Text>
+                </Animated.Text>
 
-                <Text style={{fontSize: hp(2.5)}} className='font-semibold text-neutral-800 tracking-wide mb-2'>
+                <Animated.Text entering = {FadeInDown.delay(300).springify()} style={{fontSize: hp(2.5)}} className='font-semibold text-neutral-800 tracking-wide mb-2'>
                     Instructions
-                </Text>
+                </Animated.Text>
                     {
                         item.instructions.split(',').map((instruction, index) => {
                             return(
-                                <Text key={index} style={{fontSize: hp(2)}} className='font-semibold text-neutral-700 tracking-wide mb-10'>
+                                <Animated.Text entering = {FadeInDown.delay(300).springify()} key={index} style={{fontSize: hp(2)}} className='font-semibold text-neutral-700 tracking-wide mb-10'>
                                     {index+1}. {instruction}
-                                </Text>
+                                </Animated.Text>
                             )
                         })
                     }
--- components/ExerciseCard.jsx
+++ components/ExerciseCard.jsx
@@ -7,7 +7,7 @@ import { useRouter } from "expo-router";
 
 export default function ExerciseCard({item, router, index}) {
   return (
-    <Animated.View entering={FadeInUp.delay(200).springify()} style={{maxWidth: wp(40)}}>
+    <Animated.View entering={FadeInUp.springify()} style={{maxWidth: wp(40)}}>
       <TouchableOpacity onPress={()=> router.push({pathname: '/exerciseDetails', params: item})} className='flex-1 space-y-2 '>
         <View className='bg-neutral-200 shadow rounded-lg mt-8'>
           <Image
